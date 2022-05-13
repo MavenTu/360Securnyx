9a9d7db4b3579f2ed1504063f4dedc23e3ce2d41
Soham <soham.dutta@codelogicx.com>
2022-05-13 16:40:00 +0530

    new change code

--- src/com/demo/WebTest.java
+++ src/com/demo/WebTest.java
@@ -23,34 +23,36 @@ public class WebTest {
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("(//*[@name='button'])[2]")).click();
 		Thread.sleep(4000);
+		
 		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/header[1]/nav[1]/div[1]/div[3]/ul[1]/li[1]/a[1]")).click();
-		Thread.sleep(6000);
-		driver.findElement(By.xpath("//a[normalize-space()='Flat Owners']")).click();
+		Thread.sleep(4000);
+	    driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/header[1]/nav[1]/div[1]/div[3]/ul[1]/li[1]/ul[1]/li[1]/a[1]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/button[1]/span[1]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]")).sendKeys("sla");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]")).sendKeys("das");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[3]/div[1]/input[1]")).sendKeys("savdas@yopmail.com");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[4]/div[1]/input[1]")).sendKeys("7535887740");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[6]/div[1]/div[1]/label[1]/span[1]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[7]/div[1]/input[1]")).sendKeys("DEV");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(4000);
+		
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/header[1]/nav[1]/div[1]/div[3]/ul[1]/li[1]/a[1]")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/header[1]/nav[1]/div[1]/div[3]/ul[1]/li[1]/ul[1]/li[1]/a[1]")).click();
 		Thread.sleep(6000);
 		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[2]/div[1]/div[1]")).click();
 		Thread.sleep(6000);
 		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[2]/div[1]/div[3]/ul[1]/li[4]/span[1]")).click();
 		Thread.sleep(6000);
-		
-		
-		
-		
-	    driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/button[1]")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]")).sendKeys("Sunni");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]")).sendKeys("Biswas");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[3]/div[1]/input[1]")).sendKeys("sunnidas@yopmail.com");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[4]/div[1]/input[1]")).sendKeys("8235887740");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[6]/div[1]/div[1]/label[1]/span[1]")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[7]/div[1]/input[1]")).sendKeys("QA");
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		Thread.sleep(4000);
 		
 		driver.findElement(By.xpath("//*[@class='user-image circle-image']")).click();
 		Thread.sleep(4000);
